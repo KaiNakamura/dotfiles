f9bf8e0aa7daae254845c41cd27874e01b7f9bc7
Kai Nakamura <kaihnakamura@gmail.com>
2025-11-30 13:21:20 -0500

    Change zshrc to copy instead of symlink

--- zsh/install.sh
+++ zsh/install.sh
@@ -25,17 +25,17 @@ zshrc_source="$WORKDIR/.zshrc"
 zshrc_target="$HOME/.zshrc"
 
 if [[ ! -f "$zshrc_source" ]]; then
-    echo "Warning: No .zshrc found in zsh module, skipping symlink"
+    echo "Warning: No .zshrc found in zsh module, skipping copy"
 else
     # Backup existing .zshrc if it exists
     if [[ -e "$zshrc_target" ]]; then
         echo "Backing up existing .zshrc to ${zshrc_target}.bak"
-        cp -L "$zshrc_target" "${zshrc_target}.bak"
+        mv "$zshrc_target" "${zshrc_target}.bak"
     fi
     
-    # Create symlink
-    ln -sf "$zshrc_source" "$zshrc_target"
-    echo ".zshrc symlinked to dotfiles"
+    # Copy .zshrc
+    cp "$zshrc_source" "$zshrc_target"
+    echo ".zshrc copied to dotfiles"
 fi
 
 # Change default shell to zsh
